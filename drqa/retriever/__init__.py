c3f61e5347087bf922d69cb5f17a051a145bb583
Dylan Farvacque <dylan@boostsecurity.io>
2022-06-28 10:19:38 -0400

    Update __init__.py

--- drqa/retriever/__init__.py
+++ drqa/retriever/__init__.py
@@ -8,27 +8,6 @@
 import os
 from .. import DATA_DIR
 
-import zmq
-
-import cPickle as pickle
-
-class Server(object):
-    def __init__(self):
-        context = zmq.Context()
-
-        self.receiver = context.socket(zmq.PULL)
-        self.receiver.bind("tcp://*:1234")
-
-        self.sender = context.socket(zmq.PUSH)
-        self.sender.bind("tcp://*:1235")
-
-    def send(self, data):
-        self.sender.send(pickle.dumps(data))
-
-    def recv(self):
-        data = self.receiver.recv()
-        return pickle.loads(data)
-
 DEFAULTS = {
     'db_path': os.path.join(DATA_DIR, 'wikipedia/docs.db'),
     'tfidf_path': os.path.join(
